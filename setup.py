b06e323278084d349cfa98a687486f9b4ab36c0d
Fabrice ETANCHAUD <fabrice.etanchaud@netc.fr>
2022-05-16 11:30:21 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 from setuptools import setup
 
 package_name = "dbt-dremio"
-package_version = "1.0.6.4"
+package_version = "1.0.6.5"
 description = """The dremio adapter plugin for dbt (data build tool)"""
 
 setup(
